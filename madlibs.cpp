41500a799b7786cc688a0a86e40565c45862b6c3
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-01-31 20:20:14 -0800

    changes made

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,22 @@
 
 int main()
 {
+int number;
+std::cout<<"What is a number?"<<std::endl;
+std::cin>>number;
+char symbol;
+std::cout<<"What is a symbol?"<<std::endl;
+std::cin>>symbol;
+double number2;
+std::cout<<"What is a number?"<<std::endl;
+std::cin>>number2;
+char symbol2;
+std::cout<<"What is a symbol?"<<std::endl;
+std::cin>>symbol2;
+std::cout<<"It is day "<<number<<", the humans still have not noticed.";
+std::cout<<"I ate a fish "<<symbol<<".";
+std::cout<<"I may only be able to survive "<<number2<<" days.";
+std::cout<<"I have to go now, they will be here soon "<<symbol2<<"."<<std::endl;
 
   return 0;
 }
